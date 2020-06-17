236eb61a4aff6bb640c05ff06b704d6db04b87f5
Destry Teeter <destry.teeter@gmail.com>
2020-06-17 08:34:39 -0700

    > Fix build logic

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update Carfile path
-FRAMEWORK_PATH=`find \`pwd\` -name Test2 | head -n 1`
+FRAMEWORK_PATH=`find \`pwd\` -name Test2 -not -path '*/Carthage/*' | head -n 1`
 echo 'git "'$FRAMEWORK_PATH'" "master"' > Test/Cartfile
 
 # Build Cartfile
